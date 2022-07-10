71af44b12f6577ee1d6edfdc2915431457623088
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-07-10 14:42:24 +0530

    get curr artist private route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+// Importing Profile & User Model
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
 // @route   GET api/profile/api
 // @desc    Testing profile route
 // @access  Public
 router.get("/api", (req, res) => res.json({ msg: "profile router working" }));
+
+// @route   GET api/profile
+// @desc    Get Current Artist profile route
+// @access  Private
+router.get(
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	(req, res) => {
+		const errors = {};
+		Profile.findOne({ user: req.user.id })
+			.then((profile) => {
+				if (!profile) {
+					error.noprofile = "No profile associated with this user id";
+				}
+				res.json(profile);
+			})
+			.catch((err) => res.status(404).json(err));
+	}
+);
 
 module.exports = router;
