e034c8c16129403a58a219cafaf7f09083a3e868
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-07-14 11:33:44 +0530

    restructuring of backend code

--- server.js
+++ server.js
@@ -5,20 +5,21 @@ const passport = require("passport");
 
 const user = require("./routes/api/user");
 const profile = require("./routes/api/profile");
+require("dotenv").config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// MongoDB config
-const db = require("./config/key").mongoURI;
-
 // Connect with MongoDB
 mongoose
-	.connect(db)
-	.then(() => console.log("MongoDB Atlas Connected"))
-	.catch((err) => console.log(err));
+	.connect(process.env.DATABASE, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB Connected Successfully"))
+	.catch((err) => console.log("DB Connection Error: ", err));
 
 // Middleware
 app.use(passport.initialize());
@@ -33,4 +34,4 @@ app.use("/api/profile", profile);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
